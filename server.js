a07bc6392e2349d560530efdcb6c72fa3f1d526c
Aprende Web <aprendewebnodejs@gmail.com>
2020-06-27 18:21:26 -0500

    Error handler

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const app = express();
 const peopleRouter = require('./src/peoples/routes');
 const animalRouter = require('./src/animals/routes');
 
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+  res.status(500).send(`Something wrong ${error.message}`);
+};
+
 app.use(express.json());
 
 app.use('/people', peopleRouter);
 app.use('/animal', animalRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Server listen on port', 3000);
