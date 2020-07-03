c9d0c7b6b1aa11b30f51b59560bb64797287afef
Aprende Web <aprendewebnodejs@gmail.com>
2020-07-02 19:48:28 -0500

    Adding Boom

--- src/peoples/middleware.js
+++ src/peoples/middleware.js
@@ -1,10 +1,12 @@
+const Boom = require('@hapi/boom');
+
 module.exports = (schema) => {
   return async (req, res, next) => {
     try {
       await schema.validateAsync(req.body);
       next();
     } catch (error) {
-      res.send(error.message);
+      res.send(Boom.badData(error));
     }
   };
 };
