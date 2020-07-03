e00f43c1b3c45745d2044750df24c200bf9b060b
Aprende Web <aprendewebnodejs@gmail.com>
2020-07-02 20:24:15 -0500

    Morgan y Helmet

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const peopleRouter = require('./src/peoples/routes');
 const { port } = require('./config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const errorHandler = (error, req, res, next) => {
   console.log(error.message);
   res.status(500).send(`Something wrong ${error.message}`);
 };
 
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(express.json());
-
 app.use('/people', peopleRouter);
-
 app.use(errorHandler);
 
 app.listen(port, () => {
